5a1a0fdc03a654424977e40a509f496bc0b79cc3
Aleksei Rutkovskii <70371908+rutkovskii@users.noreply.github.com>
2022-04-14 23:22:51 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = "5130740224:AAH4TaVA4vr6M-cv8XoQzdDnfU9EL_nt1Ns"
-# TestBot #"5174789806:AAFVmgC4SZAu9a5az1uBvrdNfoxkXonqhUM"
-# FlightAnalyzer #"5130740224:AAH4TaVA4vr6M-cv8XoQzdDnfU9EL_nt1Ns"
